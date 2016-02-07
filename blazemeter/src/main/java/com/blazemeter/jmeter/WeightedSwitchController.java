5385e78b578e522ab947f1f3773be297ba2b93dc
Andrey Pohilko <apc4@ya.ru>
2016-02-06 21:38:17 -0800

    Simple save & load for grid

--- blazemeter/src/main/java/com/blazemeter/jmeter/WeightedSwitchController.java
+++ blazemeter/src/main/java/com/blazemeter/jmeter/WeightedSwitchController.java
@@ -1,8 +1,27 @@
 package com.blazemeter.jmeter;
 
+import kg.apc.jmeter.JMeterPluginsUtils;
 import org.apache.jmeter.control.GenericController;
+import org.apache.jmeter.gui.util.PowerTableModel;
+import org.apache.jmeter.testelement.property.CollectionProperty;
+import org.apache.jmeter.testelement.property.JMeterProperty;
 
 import java.io.Serializable;
 
 public class WeightedSwitchController extends GenericController implements Serializable {
+    private static final String WEIGHTS = "Weights";
+
+    public void setData(PowerTableModel model) {
+        CollectionProperty prop = JMeterPluginsUtils.tableModelRowsToCollectionProperty(model, WEIGHTS);
+        setProperty(prop);
+    }
+
+    public CollectionProperty getData() {
+        JMeterProperty prop = getProperty(WEIGHTS);
+        if (prop instanceof CollectionProperty) {
+            return (CollectionProperty) prop;
+        } else {
+            return new CollectionProperty();
+        }
+    }
 }
--- blazemeter/src/main/java/com/blazemeter/jmeter/WeightedSwitchControllerGui.java
+++ blazemeter/src/main/java/com/blazemeter/jmeter/WeightedSwitchControllerGui.java
@@ -1,5 +1,7 @@
 package com.blazemeter.jmeter;
 
+import kg.apc.jmeter.JMeterPluginsUtils;
+import kg.apc.jmeter.gui.Grid;
 import org.apache.jmeter.control.Controller;
 import org.apache.jmeter.control.gui.AbstractControllerGui;
 import org.apache.jmeter.gui.GuiPackage;
@@ -7,13 +9,39 @@ import org.apache.jmeter.gui.tree.JMeterTreeModel;
 import org.apache.jmeter.gui.tree.JMeterTreeNode;
 import org.apache.jmeter.samplers.Sampler;
 import org.apache.jmeter.testelement.TestElement;
+import org.apache.jmeter.testelement.property.CollectionProperty;
+import org.apache.jmeter.testelement.property.JMeterProperty;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
+import java.awt.*;
 import java.util.LinkedList;
 
 public class WeightedSwitchControllerGui extends AbstractControllerGui {
     private static final Logger log = LoggingManager.getLoggerForClass();
+    private static final String WIKIPAGE = "WeightedSwitch";
+    public static final String WEIGHTS = "Weight";
+    private Grid grid;
+
+    public WeightedSwitchControllerGui() {
+        super();
+        init();
+        clearGui();
+    }
+
+    private void init() {
+        setLayout(new BorderLayout(0, 5));
+        setBorder(makeBorder());
+
+        add(JMeterPluginsUtils.addHelpLinkToPanel(makeTitlePanel(), WIKIPAGE), BorderLayout.NORTH);
+
+        grid = new Grid("Child Item Weights",
+                new String[]{"Name", WEIGHTS},
+                new Class[]{String.class, String.class},
+                new String[]{"", "100"});
+
+        add(grid, BorderLayout.CENTER);
+    }
 
     @Override
     public String getLabelResource() {
@@ -35,29 +63,42 @@ public class WeightedSwitchControllerGui extends AbstractControllerGui {
     @Override
     public void modifyTestElement(TestElement element) {
         super.configureTestElement(element);
-        // from GUI to model
+        if (element instanceof WeightedSwitchController) {
+            WeightedSwitchController wsc = (WeightedSwitchController) element;
+            wsc.setData(grid.getModel());
+        }
     }
 
     @Override
     public void configure(TestElement element) {
-        super.configure(element);
         // from model to GUI
+        super.configure(element);
 
         GuiPackage gp = GuiPackage.getInstance();
 
-        if (gp != null) {
+        if (gp != null && element instanceof WeightedSwitchController) {
+            WeightedSwitchController wsc = (WeightedSwitchController) element;
+            CollectionProperty oldData = wsc.getData();
+
             JMeterTreeModel treeModel = GuiPackage.getInstance().getTreeModel();
             JMeterTreeNode root = (JMeterTreeNode) treeModel.getRoot();
-            LinkedList<JMeterTreeNode> childItems = getChildItems(root, element);
-            log.info("Children found: " + childItems.size());
-            for (JMeterTreeNode node : childItems) {
-                log.warn("Child: " + node.getTestElement());
-                // TODO
+            LinkedList<JMeterTreeNode> childItems = getChildItems(root, wsc);
+            grid.getModel().clearData();
+            for (int n = 0; n < childItems.size(); n++) {
+                JMeterTreeNode node = childItems.get(n);
+                String w = "100";
+                if (oldData.size() > n && oldData.get(n) != null) {
+                    JMeterProperty row = oldData.get(n);
+                    if (row instanceof CollectionProperty) {
+                        w = ((CollectionProperty) row).get(1).getStringValue();
+                    }
+                }
+                grid.getModel().addRow(new String[]{node.getTestElement().getName(), w});
             }
         }
     }
 
-    private LinkedList<JMeterTreeNode> getChildItems(JMeterTreeNode root, TestElement element) {
+    private LinkedList<JMeterTreeNode> getChildItems(JMeterTreeNode root, WeightedSwitchController element) {
         LinkedList<JMeterTreeNode> result = new LinkedList<>();
         for (int i = 0; i < root.getChildCount(); i++) {
             JMeterTreeNode child = (JMeterTreeNode) root.getChildAt(i);
